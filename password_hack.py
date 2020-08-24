3dc660585a3cf41c6fff1cdfa79b915e7a431cf0
Ranabir Dey <ranabir4u@gmail.com>
2020-08-24 18:22:38 +0530

    Update password_hack.py

--- password_hack.py
+++ password_hack.py
@@ -7,14 +7,16 @@ def req_api_data(hashkey):
 	url = 'https://api.pwnedpasswords.com/range/' + hashkey
 	res = req.get(url)
 	if res.status_code != 200:
-		raise RuntimeError(f'error while fetching API data, error code {res.status_code}. Please check url')
+		err_msg = 'error while fetching API data, error code ' + res.status_code + '. Please check url'
+		raise RuntimeError(err_msg)
 	return res
 
 def password_leak_count(response, hash_tail):
 	password_hack = False
 	for multi_res in response.text.splitlines():
 		if multi_res.split(":")[0] == hash_tail:
-			return f'Oops!! your password has been hacked {multi_res.split(":")[1]} times. Please change your password now.'
+			ret_msg = 'Oops!! your password has been hacked ' + multi_res.split(":")[1] + ' times. Please change your password now.'
+			return ret_msg
 			password_hack = True
 	if password_hack == False:
 		return 'Congratulations! Your password is still secure'
